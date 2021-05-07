16ba7c15444b7a41c548ed60c48700447cc46b8a
andreslavado <andreslavado@seti.com.co>
2021-05-07 17:52:09 -0500

    Fist commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "keyName" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "user_data" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
